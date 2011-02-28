4229f77c8a857d8fbe5181557780252ad8d2d675
Jack Scott <jack@jackscott.id.au>
2011-02-28 19:53:06 +1100

    Something happened?

--- www-app/home.php
+++ www-app/home.php
@@ -4,11 +4,11 @@ session_start();
 if($_SESSION['username']=="")
 {
 	header("Location: ./index.php?message=not-auth");
-} else {
+}
 	//The rest of the page.
 	echo "Logged in as ".$_SESSION['username'];
 	echo "<a href=\"./logout.php\">Logout</a>";
-}
+
 
 
 ?>
