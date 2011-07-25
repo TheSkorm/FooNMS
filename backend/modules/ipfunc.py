66b1cff86ae07ad1037cf628da8e7210c3428f4c
Michael Wheeler <mwheeler@iron.grand.fl.us.fooproject.net>
2011-07-25 20:55:53 +1000

    actually does stuff now

--- backend/modules/ipfunc.py
+++ backend/modules/ipfunc.py
@@ -3,6 +3,7 @@ class ip:
 		if ipaddr.__class__ == str:
 			print "Is string"
 			#detect if ipv4 or ipv6 and convert to long which becomes self.ip
+			# this is the hard part... -_-
 		if ipaddr.__class__ == long or ipaddr.__class__ == int:
 			#if it's a long, don't change anything unless it's a v4 address, if it is add 0xffff00000000
 			#this most likely breaks a thousand RFCs but I think this will be handy. The only exception being loop back (1). I presume the rest will just be people mistakening it for a v4 input.
@@ -11,7 +12,19 @@ class ip:
 			else:
 				self.ip = ipaddr
 	def prettyprint(self):
-		return ":".join([hex(self.ip)[2:][sections:sections+4] for sections in range(0,len(hex(a))-4,4)])
+		if self.ip > 0xffff00000000 and self.ip < 0xffffffffffff:
+			hexd = hex(self.ip)[2:]
+			if hexd[-1] == "L":
+				hexd = hexd[:-1]
+			parts = [hexd[sections:sections+4] for sections in range(0,len(hexd),4)]
+			return "::ffff:" + str(int(parts[-2][:2], 16)) + "." + str(int(parts[-2][2:4], 16)) + "." + str(int(parts[-1][:2], 16)) + "." + str(int(parts[-1][2:4], 16))
+		else:
+			hexd = hex(self.ip)[2:].strip("L")
+			parts = [hexd[sections:sections+4] for sections in range(0,len(hexd),4)]
+			if len(parts)==8:
+				return ":".join(parts)
+			else:
+				return "::" + ":".join(parts)
 		#detect if IPv6 or IPv4 and print nicely
 	def bprint(self):
 		return 0
@@ -21,4 +34,8 @@ class ip:
 		#given a mask print the network
 
 
-print ip(800).ip
+print ip(0b1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111000000000000000000).prettyprint()
+
+print ip(3671509009).prettyprint()
+
+
