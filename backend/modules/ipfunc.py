ad222526535a449232a15756be7a9db5ab0d4185
TheSkorm <git@michael-wheeler.org>
2011-07-25 04:47:41 -0700

    Added TODO's - TODO automatic subnet mask detection.

--- backend/modules/ipfunc.py
+++ backend/modules/ipfunc.py
@@ -2,11 +2,13 @@ class ip:
 	def __init__(self, ipaddr):
 		if ipaddr.__class__ == str:
 			print "Is string"
-			#detect if ipv4 or ipv6 and convert to long which becomes self.ip
+			#TODO detect if ipv4 or ipv6 and convert to long which becomes self.ip
 			# this is the hard part... -_-
 		if ipaddr.__class__ == long or ipaddr.__class__ == int:
 			#if it's a long, don't change anything unless it's a v4 address, if it is add 0xffff00000000
 			#this most likely breaks a thousand RFCs but I think this will be handy. The only exception being loop back (1). I presume the rest will just be people mistakening it for a v4 input.
+
+			#TODO check is it's a IPv4 mask, and if so automatically set cidr
 			if (ipaddr < 0xFFFFFFFF) and (ipaddr > 1):
 				self.ip = ipaddr + 0xffff00000000
 			else:
@@ -14,7 +16,7 @@ class ip:
 	def prettyprint(self):
 		hexd = '%x' % (self.ip,)
 		if self.ip > 0xffff00000000 and self.ip < 0xffffffffffff:
-			parts = [hexd[sections:sections+4] for sections in range(0,len(hexd),4)] # this could be cleaned up rather than using the hex could use real numbers and shiz....
+			parts = [hexd[sections:sections+4] for sections in range(0,len(hexd),4)] #TODO this could be cleaned up rather than using the hex could use real numbers and shiz....
 			return "::ffff:" + str(int(parts[-2][:2], 16)) + "." + str(int(parts[-2][2:4], 16)) + "." + str(int(parts[-1][:2], 16)) + "." + str(int(parts[-1][2:4], 16))
 		else:
 			parts = [hexd[sections:sections+4] for sections in range(0,len(hexd),4)]
@@ -25,17 +27,16 @@ class ip:
 		#detect if IPv6 or IPv4 and print nicely
 	def bprint(self):
 		return 0
-		#return string of 1/0's for debugging
+		#TODO return string of 1/0's for debugging
 	def network(self,mask):
 		return 0
-		#given a mask return the network, and set the cidr
+		#TODO given a mask return the network, and set the cidr
 	def cidr(self, cidr):
-		# if given a cidr, return the network, and set the network mask
+		#TODO if given a cidr, return the network, and set the network mask
 		return 0
 
 
 print ip(0b1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111000000000000000000).prettyprint()
 
 print ip(3671509009).prettyprint()
-
 
